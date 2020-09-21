47ccfd06bd43f0ce1adec532aba8df1f2c274e51
lukasrahm <55479438+lukasrahm@users.noreply.github.com>
2020-09-21 20:29:19 +0200

    made a typographyelement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import {useStaticQuery, graphql} from "gatsby"
 import {
     FooterWrapper, 
     FooterSocialWrapper, 
-    FooterSocialIcons
+    FooterSocialIcons,
+    P
 } from "../elements"
 
 export const Footer = () => {
@@ -42,7 +43,7 @@ export const Footer = () => {
                     <img src={data.twitter.publicURL} alt="Twitter" />
                 </a>
             </FooterSocialIcons>
-            <p>© 2020 Lukas Rahm Company. All rights reserved.</p>
+            <P size="xSmall" color="dark3">© 2020 Lukas Rahm Company. All rights reserved.</P>
         </FooterSocialWrapper>
     </FooterWrapper>
 }
