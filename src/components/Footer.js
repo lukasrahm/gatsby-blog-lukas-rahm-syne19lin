d94444f3f5a2333298def946bda6d807f2275367
lukasrahm <55479438+lukasrahm@users.noreply.github.com>
2020-09-21 19:26:41 +0200

    Footer and nav

---
+++ src/components/Footer.js
@@ -0,0 +1,47 @@
+import React from "react"
+import {useStaticQuery, graphql} from "gatsby"
+import {
+    FooterWrapper, 
+    FooterSocialWrapper, 
+    FooterSocialIcons
+} from "../elements"
+
+export const Footer = () => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            facebook: file(relativePath: {eq: "facebook.svg"}) {
+                publicURL
+            }
+            linkedin: file(relativePath: {eq: "linkedin.svg"}) {
+                publicURL
+            }
+            instagram: file(relativePath: {eq: "instagram.svg"}) {
+                publicURL
+            }
+            twitter: file(relativePath: {eq: "twitter.svg"}) {
+                publicURL
+            }
+        }
+    `)
+
+
+    return <FooterWrapper>
+        <FooterSocialWrapper>
+            <FooterSocialIcons>
+                <a href="https://facebook.com/LukasRahm0/" target="_blank" rel="noopener noreferrer">
+                    <img src={data.facebook.publicURL} alt="Facebook" />
+                </a>
+                <a href="https://www.linkedin.com/in/lukas-rahm-13570b139/" target="_blank" rel="noopener noreferrer">
+                    <img src={data.linkedin.publicURL} alt="Linkedin" />
+                </a>
+                <a href="https://www.instagram.com/lukasrahm/" target="_blank" rel="noopener noreferrer">
+                    <img src={data.instagram.publicURL} alt="Instagram" />
+                </a>
+                <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={data.twitter.publicURL} alt="Twitter" />
+                </a>
+            </FooterSocialIcons>
+        </FooterSocialWrapper>
+    </FooterWrapper>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Container } from "../components"
 const IndexPage = () => {
   return (
     <Container>
-      <h1>Hello Worldl!</h1>
+      
     </Container>
   )
 }
